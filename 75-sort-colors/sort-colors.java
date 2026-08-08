978b7c3221957b64ee82410488f8fe80d47763e4
prince-dhillon <princedhillon.ai@gmail.com>
2026-08-08 13:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (61.19%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        // Using normal Method
+        int zero=0;
+        int one=0;
+        int two=0;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                zero++;
+            }
+            else if(nums[i]==1){
+                one++;
+            }
+            else{
+                two++;
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(i<zero){
+                nums[i] = 0;
+            }
+            else if(i<zero+one){
+                nums[i] = 1;
+            }
+            else{
+                nums[i] = 2;
+            }
+        }
+        
+    }
+}
