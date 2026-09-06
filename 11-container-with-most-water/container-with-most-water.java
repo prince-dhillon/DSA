7130dfa7440246b546b072dfc592c402b4f393a7
prince-dhillon <princedhillon.ai@gmail.com>
2026-09-06 15:51:20 +0530

    Time: 4 ms (94.99%) | Memory: 77.5 MB (29.39%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0;
+        int j=height.length-1;
+        int max = 0;
+        while(i<j){
+            int min = Math.min(height[i],height[j]);
+            if(min*(j-i)>max){
+                max = min*(j-i);
+            }
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
+}
