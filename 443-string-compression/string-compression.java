dd67091d08fb65f1b7819961bf982d8d5cf982cd
prince-dhillon <princedhillon.ai@gmail.com>
2026-09-14 09:53:44 +0530

    Time: 2 ms (28.41%) | Memory: 45.4 MB (49.27%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,23 +1,24 @@
 class Solution {
     public int compress(char[] chars) {
         StringBuilder ans = new StringBuilder("");
-        int i=0; int j=0;
+        int i=0;
+        int j=0;
         while(j<chars.length){
             if(chars[i]==chars[j]){
                 j++;
             }
             else{
                 ans.append(chars[i]);
-                if((j-i)>1){
-                    ans.append(j-i);
+                if(j-i>1){
+                ans.append(j-i);
                 }
                 i=j;
             }
         }
         ans.append(chars[i]);
-                if((j-i)>1){
-                    ans.append(j-i);
-                }
+            if(j-i>1){
+            ans.append(j-i);
+            }
         for(i=0; i<ans.length(); i++){
             chars[i] = ans.charAt(i);
         }
