298848506a66b7627de80abf980fa6902014a533
prince-dhillon <princedhillon.ai@gmail.com>
2026-09-18 10:29:31 +0530

    Time: 117 ms (5.21%) | Memory: 47.8 MB (57.28%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
-        int j=1;
-        while(j<nums.length){
-            if(nums[i]!=0){
-                i++;
-                j++;
+        int n = 0;
+        for(int num: nums){
+            if(num==0){
+                n++;
             }
-            else if(nums[j]==0){
-                j++;
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<nums.length-i-1; j++){
+                if(nums[j]==0){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
             }
-            else{
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-                j++;
-            }
+
         }
     }
 }
